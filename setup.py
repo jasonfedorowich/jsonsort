9f3c940774e112ceb6b593736484ee8c039cae48
Jason Fedorowich <jasonfedorowich@gmail.com>
2022-03-06 15:55:47 -0500

    -changed setup file with long description

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import setuptools
+
 setuptools.setup(name='json_sorter',
-version='0.1',
+version='0.1.1',
 description='Simple package to Sort Json Content',
 license='MIT',
 url='https://github.com/jasonfedorowich/jsonsort',
@@ -10,4 +11,5 @@ author_email='jason.fedorowich@gmail.com',
 python_requires='>=3',
 packages=setuptools.find_packages(),
 zip_safe=False,
-long_description="Package to sort Json from files, strings or dictionary for usage see: https://github.com/jasonfedorowich/jsonsort" )
+long_description=open('README.md', 'r').read(),
+long_description_content_type="")
